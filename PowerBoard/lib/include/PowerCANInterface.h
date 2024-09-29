9578abef0297ff10cd353132adb9a1f2564a5b68
TamerTimraz <tamertimraz04@gmail.com>
2024-09-29 15:27:17 -0400

    Created PowerCANInterface header and cpp files

---
+++ PowerBoard/lib/include/PowerCANInterface.h
@@ -0,0 +1,18 @@
+#ifndef POWER_CAN_INTERFACE_H
+#define POWER_CAN_INTERFACE_H
+
+// #include "BPSCANStructs.h"
+#include "CANInterface.h"
+// #include "ECUCANStructs.h"
+// #include "MotorControllerCANStructs.h"
+
+class PowerCANInterface : public CANInterface {
+  public:
+    PowerCANInterface(PinName rd, PinName td, PinName standby_pin);
+    int send(CANStruct *can_struct);
+
+  private:
+    void message_handler() override;
+};
+
+#endif
