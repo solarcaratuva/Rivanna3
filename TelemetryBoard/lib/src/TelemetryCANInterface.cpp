40a7ebae033ab52245a298f8677dce5791ff3676
Ethan Coon <eethanccoon@gmail.com>
2024-11-17 15:18:46 -0500

    Added all message ids to message handler

--- TelemetryBoard/lib/src/TelemetryCANInterface.cpp
+++ TelemetryBoard/lib/src/TelemetryCANInterface.cpp
@@ -1,6 +1,7 @@
 #include "TelemetryCANInterface.h"
 #include "MotorControllerCANStructs.h"
 #include "log.h"
+#include "CANStructs.h"
 
 // Constructor for TelemetryCANInterface.h that extends from CANInterface.h in Common include
 TelemetryCANInterface::TelemetryCANInterface(PinName rd, PinName td, PinName standby_pin)
@@ -49,16 +50,59 @@ void TelemetryCANInterface::message_handler() {
             CANInterface::write_CAN_message_data_to_buffer(message_data,
                                                            &message);
             log_debug("Received CAN message with ID 0x%03X Length %d Data 0x%s ", message.id, message.len, message_data);
-            if (message.id == BPSError_MESSAGE_ID) { //deserializes messages and handles them
-                BPSError can_struct;
+            if (message.id == ECUPowerAuxCommands_MESSAGE_ID) {
+                ECUPowerAuxCommands can_struct;
                 can_struct.deserialize(&message);
                 handle(&can_struct);
-            } else if (message.id == MotorControllerPowerStatus_AUX_BUS_MESSAGE_ID) {
+            } else if (message.id == PowerAuxError_MESSAGE_ID) {
+                PowerAuxError can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == SolarCurrent_MESSAGE_ID) {
+                SolarCurrent can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == SolarTemp_MESSAGE_ID) {
+                SolarTemp can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == SolarVoltage_MESSAGE_ID) {
+                SolarVoltage can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == SolarPhoto_MESSAGE_ID) {
+                SolarPhoto can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == MotorControllerPowerStatus_MESSAGE_ID || message.id == MotorControllerPowerStatus_AUX_BUS_MESSAGE_ID) {
+                log_debug("Motor Power Status");
                 MotorControllerPowerStatus can_struct;
                 can_struct.deserialize(&message);
                 handle(&can_struct);
-            } else if(message.id == ECUPowerAuxCommands_MESSAGE_ID) {
-                ECUPowerAuxCommands can_struct;
+            } else if (message.id == MotorControllerDriveStatus_MESSAGE_ID || message.id == MotorControllerDriveStatus_AUX_BUS_MESSAGE_ID) {
+                log_debug("Motor Drive Status");
+                MotorControllerDriveStatus can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == MotorControllerError_MESSAGE_ID || message.id == MotorControllerError_AUX_BUS_MESSAGE_ID) {
+                log_debug("Motor Error Status");
+                MotorControllerError can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == BPSPackInformation_MESSAGE_ID) {
+                BPSPackInformation can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == BPSError_MESSAGE_ID) {
+                BPSError can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == BPSCellVoltage_MESSAGE_ID) {
+                BPSCellVoltage can_struct;
+                can_struct.deserialize(&message);
+                handle(&can_struct);
+            } else if (message.id == BPSCellTemperature_MESSAGE_ID) {
+                BPSCellTemperature can_struct;
                 can_struct.deserialize(&message);
                 handle(&can_struct);
             }
