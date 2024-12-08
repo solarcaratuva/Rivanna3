3585c436aa2cef0c6e70c2af81d1d947d780dd14
Luke Shee <eer8sc@virginia.edu>
2024-12-08 20:16:58 +0000

    Added basic handle methods in TelemetryBoard main.cpp

--- TelemetryBoard/lib/include/TelemetryCANInterface.h
+++ TelemetryBoard/lib/include/TelemetryCANInterface.h
@@ -9,9 +9,19 @@
 class TelemetryCANInterface : public CANInterface {
   public:
     TelemetryCANInterface(PinName rd, PinName td, PinName standby_pin);
-    void handle(BPSError *can_struct);
-    void handle(MotorControllerPowerStatus *can_struct);
     void handle(ECUPowerAuxCommands *can_struct);
+    void handle(PowerAuxError *can_struct);
+    void handle(SolarCurrent *can_struct);
+    void handle(SolarTemp *can_struct);
+    void handle(SolarVoltage *can_struct);
+    void handle(SolarPhoto *can_struct);
+    void handle(MotorControllerPowerStatus *can_struct);
+    void handle(MotorControllerDriveStatus *can_struct);
+    void handle(MotorControllerError *can_struct);
+    void handle(BPSPackInformation *can_struct);
+    void handle(BPSError *can_struct);
+    void handle(BPSCellVoltage *can_struct);
+    void handle(BPSCellTemperature *can_struct);
     int send(CANStruct *can_struct);
 
   private:
---
+++ TelemetryBoard/src/main.cpp
@@ -0,0 +1,56 @@
+#include "TelemetryCANInterface.h"
+#include "ThisThread.h"
+#include "log.h"
+#include <mbed.h>
+
+void TelemetryCANInterface::handle(ECUPowerAuxCommands *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(PowerAuxError *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(SolarCurrent *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(SolarTemp *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(SolarVoltage *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(SolarPhoto *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(MotorControllerPowerStatus *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(MotorControllerDriveStatus *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(MotorControllerError *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(BPSPackInformation *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(BPSError *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(BPSCellVoltage *can_struct) {
+    
+}
+
+void TelemetryCANInterface::handle(BPSCellTemperature *can_struct) {
+    
+}
