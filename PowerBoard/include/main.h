67efd6bd8e078be3f9a4523993fe03e035f044b8
Colby <bpx9hy@virginia.edu>
2024-12-19 13:42:27 -0500

    added include guard

--- PowerBoard/include/main.h
+++ PowerBoard/include/main.h
@@ -1,5 +1,10 @@
+#ifndef MAIN_POWERBOARD_H // include guard
+#define MAIN_POWERBOARD_H
+
 #include <mbed.h>
 #include <rtos.h>
 
 extern AnalogIn throttle_pedal;
 extern AnalogIn brake_pedal;
+
+#endif
