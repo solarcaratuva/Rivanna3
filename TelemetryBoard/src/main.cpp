78a3a106133d708cb68d8c051ff2c475b754e850
Ethan Coon <eethanccoon@gmail.com>
2025-02-22 18:14:56 +0000

    Redid code for sending XBEE messages

--- TelemetryBoard/src/main.cpp
+++ TelemetryBoard/src/main.cpp
@@ -1,38 +1,34 @@
 #include "mbed.h"
 #include <string.h>
-#include "pindef.h"
+#include "pindef.h" 
 
 using namespace std::chrono;
 
-BufferedSerial xbee(Radio_TX, Radio_RX, 9600);  // XBee radio interface: TX, RX, baud rate 9600
-BufferedSerial pc(USB_TX, USB_RX, 115200);  // Debug console over USB
+BufferedSerial xbee(RADIO_TX, RADIO_RX, 9600);
+BufferedSerial pc(USB_TX, USB_RX, 115200);        // Debug console over USB
 
 int main() {
-    // Configure both serial ports to use 8 data bits, no parity, 1 stop bit.
+    // 8 data bits, no parity, 1 stop bit.
     xbee.set_format(8, BufferedSerial::None, 1);
     pc.set_format(8, BufferedSerial::None, 1);
 
+    const char *msg = "XBee Test Message\r\n";
     char buffer[128] = {0};
 
     while (true) {
-        const char *msg = "XBee Test Message\r\n";
-        // Send test message to XBee
         xbee.write(msg, strlen(msg));
-        // Also print message on the debug console
         pc.write(msg, strlen(msg));
 
-        // Wait up to 500ms for a response from XBee
+        // check for any response from the XBee module and echo to PC console if needed
         auto start = chrono::steady_clock::now();
         while (chrono::steady_clock::now() - start < 500ms) {
             if (xbee.readable()) {
                 int n = xbee.read(buffer, sizeof(buffer));
                 if (n > 0) {
-                    // Echo any received data to the debug console
                     pc.write(buffer, n);
                 }
             }
         }
-        // Wait 1 second before sending the next test message
-        ThisThread::sleep_for(1000ms);
+        ThisThread::sleep_for(1s);
     }
 }
