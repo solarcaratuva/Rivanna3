64efe95bde30f4f519029b5a11964c28598d953f
Ethan Coon <eethanccoon@gmail.com>
2025-02-27 22:38:07 +0000

    Readded because of 'extenuating circumstances'

--- TelemetryBoard/lib/src/EEPROMDriver.cpp
+++ TelemetryBoard/lib/src/EEPROMDriver.cpp
@@ -17,6 +17,7 @@ void EEPROMDriver::write_byte(uint32_t address, uint8_t data) {
     write_enable();
     _cs = 0;
     _spi.write(0x02); // WRITE command (0x02)
+    _spi.write((address >> 16) & 0xFF);
     _spi.write((address >> 8) & 0xFF);
     _spi.write(address & 0xFF);
     _spi.write(data);
@@ -27,6 +28,7 @@ void EEPROMDriver::write_byte(uint32_t address, uint8_t data) {
 uint8_t EEPROMDriver::read_byte(uint32_t address) {
     _cs = 0;
     _spi.write(0x03);
+    _spi.write((address >> 16) & 0xFF);
     _spi.write((address >> 8) & 0xFF);
     _spi.write(address & 0xFF);
     // Perform a dummy write to receive the data byte
