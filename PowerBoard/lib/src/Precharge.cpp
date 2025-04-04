3b70bea2671caf07ac228e56e65fb60bb1f3c649
Colby <bpx9hy@virginia.edu>
2025-04-04 16:47:25 -0400

    cleanup

--- PowerBoard/lib/src/Precharge.cpp
+++ PowerBoard/lib/src/Precharge.cpp
@@ -4,15 +4,17 @@
 #include "main.h"
 
 
-#define PRECHARGE_CHARGING      2500ms
-#define SLEEP_TICK              10ms
-#define CONT_12_LOGIC_THRESHOLD 0.1f // below is logic low, above is logic high
+#define PRECHARGE_CHARGING          2500ms
+#define SLEEP_TICK                  10ms
+#define CONT_12_LOGIC_THRESHOLD     0.1f // below is logic low, above is logic high
+#define HAL_EFFECT_SENSOR_SCALE     (5.0f / 3.28f) // used to scale voltages up to 5V
+#define PACK_VOLT_PCT               0.95f // precharge can proceed once RC voltage exceeds this percent of pack voltage
 
 
 // checks if a fault occured
 // if so, it will disable the precharge relays and wait forever
 void fault_trap(){
-    if (has_faulted || cont_12.read_voltage() < CONT_12_LOGIC_THRESHOLD) {
+    if (has_faulted || cont_12.read() < CONT_12_LOGIC_THRESHOLD) {
         discharge_en.write(0);
         motor_precharge_en.write(0);
         ThisThread::sleep_for(0xFFFFFFFFms); // wait forever
@@ -24,7 +26,7 @@ void fault_trap(){
 void motor_precharge(){
     log_debug("Motor precharge starting");
 
-    while (cont_12.read_voltage() < CONT_12_LOGIC_THRESHOLD || !discharge_relay_status){
+    while (cont_12.read() < CONT_12_LOGIC_THRESHOLD || !discharge_relay_status){
         ThisThread::sleep_for(SLEEP_TICK);
     }
 
@@ -32,7 +34,7 @@ void motor_precharge(){
     // start motor precharge
     motor_precharge_en.write(1);
 
-    while (rc_voltage_motor.read_voltage() < 0.95 * pack_voltage) {
+    while (rc_voltage_motor.read_voltage() * HAL_EFFECT_SENSOR_SCALE < PACK_VOLT_PCT * pack_voltage) {
         ThisThread::sleep_for(SLEEP_TICK);
         fault_trap();
     }
@@ -61,7 +63,7 @@ void motor_precharge(){
 void mppt_precharge(){
     log_debug("MPPT precharge starting");
 
-    while(cont_12.read_voltage() < CONT_12_LOGIC_THRESHOLD || !charge_relay_status){
+    while(cont_12.read() < CONT_12_LOGIC_THRESHOLD || !charge_relay_status){
         ThisThread::sleep_for(SLEEP_TICK);
     }
 
@@ -69,7 +71,7 @@ void mppt_precharge(){
     // start motor precharge
     mppt_precharge_en.write(1);
 
-    while (rc_voltage_battery.read_voltage() < 0.95 * pack_voltage) {
+    while (rc_voltage_battery.read_voltage() * HAL_EFFECT_SENSOR_SCALE < PACK_VOLT_PCT * pack_voltage) {
         ThisThread::sleep_for(SLEEP_TICK);
         fault_trap();
     }
