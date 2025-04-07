307296e4d636cfa25b18e510c57cea2d6113c4a5
Anjini Verdia <anjini.verdia@gmail.com>
2025-04-07 12:02:14 -0400

    ReadPedals.cpp updated

--- PowerBoard/lib/src/ReadPedals.cpp
+++ PowerBoard/lib/src/ReadPedals.cpp
@@ -3,20 +3,13 @@
 #include "main.h"
 #include "ReadPedals.h"
 
-#define THROTTLE_LOW_VOLTAGE         0.66
-#define THROTTLE_LOW_VOLTAGE_BUFFER  0.20
-#define THROTTLE_HIGH_VOLTAGE        3.08
-#define THROTTLE_HIGH_VOLTAGE_BUFFER 0.10
+#define SCALING_FACTOR  (3.3/3.28)
 
 
 
 // Reads the throttle pedal value and returns a uint16_t
 uint16_t read_throttle() {
-    float adjusted_throttle_input =
-        ((throttle_pedal.read_voltage() - THROTTLE_LOW_VOLTAGE -
-          THROTTLE_LOW_VOLTAGE_BUFFER) /
-         (THROTTLE_HIGH_VOLTAGE - THROTTLE_HIGH_VOLTAGE_BUFFER -
-          THROTTLE_LOW_VOLTAGE - THROTTLE_LOW_VOLTAGE_BUFFER));
+    float adjusted_throttle_input = throttle_pedal.read_voltage() * SCALING_FACTOR;
     if (adjusted_throttle_input <= 0.0f) {
         return 0;
     } else if (adjusted_throttle_input >= 1.0f) {
@@ -28,11 +21,7 @@ uint16_t read_throttle() {
 
 // TODO Reads the brake pedal value and returns a uint16_t
 uint16_t read_brake() {
-    float adjusted_brake_input =
-        ((brake_pedal.read_voltage() - THROTTLE_LOW_VOLTAGE -
-          THROTTLE_LOW_VOLTAGE_BUFFER) /
-         (THROTTLE_HIGH_VOLTAGE - THROTTLE_HIGH_VOLTAGE_BUFFER -
-          THROTTLE_LOW_VOLTAGE - THROTTLE_LOW_VOLTAGE_BUFFER));
+    float adjusted_brake_input = brake_pedal.read_voltage() * SCALING_FACTOR;
     if (adjusted_brake_input <= 0.0f) {
         return 0;
     } else if (adjusted_brake_input >= 1.0f) {
