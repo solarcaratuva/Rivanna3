2f182c120f7aaf85203a386ba40788b7272eabab
Arleee1 <keg9ve@virginia.edu>
2025-04-10 20:46:33 +0000

    precharge fixes, replace current based wait with time based wait

--- PowerBoard/lib/src/Precharge.cpp
+++ PowerBoard/lib/src/Precharge.cpp
@@ -9,6 +9,7 @@
 #define CONT_12_LOGIC_THRESHOLD     0.1f // below is logic low, above is logic high
 #define HAL_EFFECT_SENSOR_SCALE     (5.0f / 3.28f) // used to scale voltages up to 5V
 #define PACK_VOLT_PCT               0.95f // precharge can proceed once RC voltage exceeds this percent of pack voltage
+#define PRECHARGE_WAIT              3s
 
 
 // checks if a fault occured
@@ -17,7 +18,9 @@ void fault_trap(){
     if (has_faulted || cont_12.read() < CONT_12_LOGIC_THRESHOLD) {
         discharge_en.write(0);
         motor_precharge_en.write(0);
-        ThisThread::sleep_for(0xFFFFFFFFms); // wait forever
+        while(true) {
+            ThisThread::sleep_for(0xFFFFFFFFms); // wait forever
+        }
     }
 }
 
@@ -34,10 +37,13 @@ void motor_precharge(){
     // start motor precharge
     motor_precharge_en.write(1);
 
-    while (rc_voltage_motor.read_voltage() * HAL_EFFECT_SENSOR_SCALE < PACK_VOLT_PCT * pack_voltage) {
-        ThisThread::sleep_for(SLEEP_TICK);
-        fault_trap();
-    }
+    // invert rc voltage
+    // while (pack_voltage == 0 && rc_voltage_motor.read_voltage() * HAL_EFFECT_SENSOR_SCALE < PACK_VOLT_PCT * pack_voltage) {
+    //     ThisThread::sleep_for(SLEEP_TICK);
+    //     fault_trap();
+    // }
+
+    ThisThread::sleep_for(PRECHARGE_WAIT);
 
     discharge_en.write(1);
 
@@ -71,10 +77,12 @@ void mppt_precharge(){
     // start motor precharge
     mppt_precharge_en.write(1);
 
-    while (rc_voltage_battery.read_voltage() * HAL_EFFECT_SENSOR_SCALE < PACK_VOLT_PCT * pack_voltage) {
-        ThisThread::sleep_for(SLEEP_TICK);
-        fault_trap();
-    }
+    // while (pack_voltage == 0 && rc_voltage_battery.read_voltage() * HAL_EFFECT_SENSOR_SCALE < PACK_VOLT_PCT * pack_voltage) {
+    //     ThisThread::sleep_for(SLEEP_TICK);
+    //     fault_trap();
+    // }
+
+    ThisThread::sleep_for(PRECHARGE_WAIT);
 
     charge_en.write(1);
 
