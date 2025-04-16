5b1823cedf00d194ebfd0c02a66badf7ad251c71
Colby <bpx9hy@virginia.edu>
2025-04-15 20:59:20 -0400

    works

--- compile.py
+++ compile.py
@@ -3,6 +3,8 @@ import subprocess
 
 CONTAINER = "Rivanna3_compile"
 
+command = "cd Rivanna2/ && ./compile.sh"
+
 process = subprocess.run("docker --help", shell=True, capture_output=True, text=True)
 if process.returncode != 0:
     print("Docker is not running. Please start Docker and try again.")
@@ -13,12 +15,11 @@ if process.returncode != 0:
     print(f"Failed to start {CONTAINER}")
     exit(1)
 
-process = subprocess.run(f"docker exec {CONTAINER} 'cd Rivanna2/ && ./compile.sh'", shell=True, capture_output=False, text=True)
-if process.returncode != 0:
-    print(f"Failed to run command in {CONTAINER}")
-    exit(1)
+compile_process = subprocess.run(f"docker exec {CONTAINER} /bin/bash -c \"{command}\"", shell=True, capture_output=False, text=True)
 
 process = subprocess.run(f" docker stop {CONTAINER}", shell=True, capture_output=True, text=True)
 if process.returncode != 0:
     print(f"Failed to stop {CONTAINER}")
     exit(1)
+
+exit(compile_process.returncode)
