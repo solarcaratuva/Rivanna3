8be520a467e80a40dad32bd4e114c7af030a28df
Colby <bpx9hy@virginia.edu>
2025-04-15 21:32:25 -0400

    finished script

--- compile.py
+++ compile.py
@@ -1,9 +1,13 @@
 import subprocess
+import argparse
 
 
 CONTAINER = "Rivanna3_compile"
 
-command = "cd Rivanna2/ && ./compile.sh"
+arg_parser = argparse.ArgumentParser(description="Compile Rivanna3 code in the Docker container.")
+arg_parser.add_argument("args", nargs="*", help="Arguments to pass to `compile.sh`.")
+arg_parser.add_argument("-c", "--clean", action="store_true", help="Clean the build before compiling.")
+args = arg_parser.parse_args()
 
 process = subprocess.run("docker --help", shell=True, capture_output=True, text=True)
 if process.returncode != 0:
@@ -15,7 +19,15 @@ if process.returncode != 0:
     print(f"Failed to start {CONTAINER}")
     exit(1)
 
-compile_process = subprocess.run(f"docker exec {CONTAINER} /bin/bash -c \"{command}\"", shell=True, capture_output=False, text=True)
+if args.clean:
+    command = "rm -rf Rivanna2/cmake_build/"
+    process = subprocess.run(f"docker exec -t {CONTAINER} /bin/bash -c \"{command}\"", shell=True, capture_output=True, text=True)
+    if process.returncode != 0:
+        print("Failed to clean the build.")
+        exit(1)
+
+command = f"cd Rivanna2/ && ./compile.sh {' '.join(args.args)}"
+compile_process = subprocess.run(f"docker exec -t {CONTAINER} /bin/bash -c \"{command}\"", shell=True, capture_output=False, text=True)
 
 process = subprocess.run(f" docker stop {CONTAINER}", shell=True, capture_output=True, text=True)
 if process.returncode != 0:
