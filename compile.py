bb99a12a4b2b22d1692185d0686e01f1504df19c
Colby <bpx9hy@virginia.edu>
2025-04-15 21:43:46 -0400

    added silent mode

--- compile.py
+++ compile.py
@@ -7,6 +7,7 @@ CONTAINER = "Rivanna3_compile"
 arg_parser = argparse.ArgumentParser(description="Compile Rivanna3 code in the Docker container.")
 arg_parser.add_argument("args", nargs="*", help="Arguments to pass to `compile.sh`.")
 arg_parser.add_argument("-c", "--clean", action="store_true", help="Clean the build before compiling.")
+arg_parser.add_argument("-s", "--silent", action="store_true", help="Suppress output from the compilation process.")
 args = arg_parser.parse_args()
 
 process = subprocess.run("docker --help", shell=True, capture_output=True, text=True)
@@ -27,7 +28,9 @@ if args.clean:
         exit(1)
 
 command = f"cd Rivanna2/ && ./compile.sh {' '.join(args.args)}"
-compile_process = subprocess.run(f"docker exec -t {CONTAINER} /bin/bash -c \"{command}\"", shell=True, capture_output=False, text=True)
+compile_process = subprocess.run(f"docker exec -t {CONTAINER} /bin/bash -c \"{command}\"", shell=True, capture_output=args.silent, text=True)
+if args.silent and compile_process.returncode != 0:
+    print("Compilation failed.")
 
 process = subprocess.run(f" docker stop {CONTAINER}", shell=True, capture_output=True, text=True)
 if process.returncode != 0:
