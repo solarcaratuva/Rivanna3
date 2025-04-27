bc53d8b1f3557616247cdf5d7e93ded9d9cb16e4
root <root@AnjiniV.>
2025-04-27 15:32:40 -0400

    #DEFINE changes to aux battery

--- PowerBoard/lib/src/AUXBattery.cpp
+++ PowerBoard/lib/src/AUXBattery.cpp
@@ -4,6 +4,10 @@
 #include "PowerCANInterface.h"
 #include "main.h"
 
+#DEFINE SCALING_FACTOR (3.3/3.23)
+#DEFINE NO_VOLTAGE 9.0f
+#DEFINE PERCENTAGE_SCALING 3.0f * 255.0f
+
 
 
 
@@ -11,12 +15,13 @@
 void update_aux_battery() {
     AuxBatteryStatus battery_status = AuxBatteryStatus();
 
-    float voltage = aux_battery.read() * 12.0f; //scale to 0–12V initially
+    float voltage = aux_battery.read() * 12.0f * SCALING_FACTOR; //scale to 0–12V initially
+    
     //set to mV
     battery_status.aux_voltage = static_cast<uint16_t>(voltage * 1000.0f); 
 
     //9-12 volt range
-    float percent_full = (voltage - 9.0f) / 3.0f * 255.0f; 
+    float percent_full = (voltage - NO_VOLTAGE) / PERCENTAGE_SCALING; 
     if (percent_full < 0) percent_full = 0;
     if (percent_full > 255) percent_full = 255; //hex percent based
 
