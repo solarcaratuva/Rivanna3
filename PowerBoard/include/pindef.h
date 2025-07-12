6dffed7441e3d6f57d7ac54e5176bb09bee74eef
Colby <bpx9hy@virginia.edu>
2025-07-12 13:28:33 -0400

    compilation update

--- PowerBoard/include/pindef.h
+++ PowerBoard/include/pindef.h
@@ -1,87 +1,8 @@
 #ifndef PINDEF_H
 #define PINDEF_H
 
-// #ifdef TARGET_POWER_BOARD // if compiling for PowerBoard
+#ifdef TARGET_NUCLEO_F413ZH // if compiling for PowerBoard
 
-// #define DEBUG_BTN       PC_13
-// #define DEBUG_LED_1     PC_1
-// #define DEBUG_LED_2     PC_2
-// #define STROBE_EN       PC_2
-// #define BRAKE_LIGHT_EN  PC_3
-// #define RIGHT_TURN_EN   PA_0
-// #define LEFT_TURN_EN    PA_1
-// #define DRL_EN          PA_2
-
-// #define THROTTLE_WIPER  PA_3
-// #define BRAKE_WIPER     PA_4
-
-// #define AUX             PB_11
-// #define CONT_12         PB_12
-// #define MPPT_HAL_SENSE  PB_13
-// #define MTR_HAL_SENSE   PB_14
-
-// #define DIS_CHARGE_EN   PB_15
-// #define MTR_PRE_EN      PC_6
-// #define CHARGE_EN       PC_7
-// #define MPPT_PRE_EN     PC_8
-
-// #define MTR_SDA         PA_8
-// #define MTR_SCL         PA_9
-
-// #define MTR_CAN_RX      PA_11
-// #define MTR_CAN_TX      PA_12
-
-// #define MAIN_CAN_STBY   PB_4
-// #define MAIN_CAN_RX     PB_5
-// #define MAIN_CAN_TX     PB_6
-
-// #define DEBUG_SWITCH    PB_9
-
-
-// #else // if not compiling for PowerBoard
-
-// #define DEBUG_BTN       NC
-// #define DEBUG_LED_1     NC
-// #define DEBUG_LED_2     NC
-// #define STROBE_EN       NC
-// #define BRAKE_LIGHT_EN  NC
-// #define RIGHT_TURN_EN   NC
-// #define LEFT_TURN_EN    NC
-// #define DRL_EN          NC
-
-// #define THROTTLE_WIPER  NC
-// #define BRAKE_WIPER     NC
-
-// #define AUX             NC
-// #define CONT_12         NC
-// #define MPPT_HAL_SENSE  NC
-// #define MTR_HAL_SENSE   NC
-
-// #define DIS_CHARGE_EN   NC
-// #define MTR_PRE_EN      NC
-// #define CHARGE_EN       NC
-// #define MPPT_PRE_EN     NC
-
-// #define MTR_SDA         NC
-// #define MTR_SCL         NC
-
-// #define MTR_CAN_RX      NC
-// #define MTR_CAN_TX      NC
-
-// #define MAIN_CAN_STBY   NC
-// #define MAIN_CAN_RX     NC
-// #define MAIN_CAN_TX     NC
-
-// #define PACK_VOLTAGE    NC
-
-// #define DEBUG_SWITCH    NC
-
-// #endif
-// #endif
-
-
-
-// #define DEBUG_BTN       
 #define DEBUG_LED_1         PE_11
 // #define DEBUG_LED_2         PE_15
 #define STROBE_EN           PF_13
@@ -113,6 +34,45 @@
 #define MAIN_CAN_RX         PB_12
 #define MAIN_CAN_TX         PB_13
 
-// #define DEBUG_SWITCH   
+
+#else // if not compiling for PowerBoard
+
+#define DEBUG_BTN       NC
+#define DEBUG_LED_1     NC
+#define DEBUG_LED_2     NC
+#define STROBE_EN       NC
+#define BRAKE_LIGHT_EN  NC
+#define RIGHT_TURN_EN   NC
+#define LEFT_TURN_EN    NC
+#define DRL_EN          NC
+
+#define THROTTLE_WIPER  NC
+#define BRAKE_WIPER     NC
+
+#define AUX             NC
+#define CONT_12         NC
+#define MPPT_HAL_SENSE  NC
+#define MTR_HAL_SENSE   NC
+
+#define DIS_CHARGE_EN   NC
+#define MTR_PRE_EN      NC
+#define CHARGE_EN       NC
+#define MPPT_PRE_EN     NC
+
+#define MTR_SDA         NC
+#define MTR_SCL         NC
+
+#define MTR_CAN_RX      NC
+#define MTR_CAN_TX      NC
+
+#define MAIN_CAN_STBY   NC
+#define MAIN_CAN_RX     NC
+#define MAIN_CAN_TX     NC
+
+#define PACK_VOLTAGE    NC
+
+#define DEBUG_SWITCH    NC
 
 #endif
+#endif
+
--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 if [ $# -eq 0 ]; then
     echo "Compiling for Rivanna3 boards"
     mbed-tools compile -m NUCLEO_F413ZH -t GCC_ARM
+    mbed-tools compile -m POWER_BOARD -t GCC_ARM
     exit $?
 fi
 
