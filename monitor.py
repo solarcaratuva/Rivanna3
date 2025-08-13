000b8407e6fdf73970a69165c956b7829ddb018e
Colby Wise <cwise1080@gmail.com>
2025-08-13 13:10:34 -0400

    colorized monitor script

--- monitor.py
+++ monitor.py
@@ -14,7 +14,7 @@ if OS != "Linux":
         from serial import Serial
         import serial.tools.list_ports
     except ImportError:
-        print(f"ERROR: 'pyserial' is not installed on {OS}. Please install it using 'pip install pyserial' on {OS}.")
+        print(f"ERROR: 'pyserial' is not installed. Please install it using 'pip install pyserial'. If on Windows, install pyserial on Windows not WSL.")
         sys.exit(1)
 
 
@@ -40,8 +40,23 @@ def copy_file_to_windows(wsl_path: str) -> None:
     path = "/" + os.path.join("mnt", "c", "Windows", "Temp", "monitor.py")
     shutil.copy(wsl_path, path)
 
+def colorize(text: str) -> str:
+    if len(text) < 20:
+        return text
+    front = text[5:20]
+
+    if "ERROR" in front or "FAULT" in front:
+        return f"\033[91m{text}\033[0m" # red
+    elif "WARNING" in front:
+        return f"\033[93m{text}\033[0m" # yellow
+    elif "DEBUG" in front:
+        return f"\033[94m{text}\033[0m" # blue
+    else:
+        return text
+
 def token_in_line(line: str, tokens: str) -> bool:
-    token_list = tokens.split("|")
+    token_list = tokens.lower().split("|")
+    line = line.lower()
     for token in token_list:
         if token in line:
             return True
@@ -62,25 +77,38 @@ def log(args, port: str) -> None:
     print(f"Serial connection to {port} established. Now listening...")
 
     messageCount = 0
+    filteredCount = 0
     errorCount = 0
 
     try:
         while True:
+            # read a line from the serial port
             try:
                 text = ser.readline().decode("utf-8").strip()
                 messageCount += 1
             except Exception as e:
                 text = f"EXCEPTION THROWN: {e}"
                 errorCount += 1
-            if args.filter and not token_in_line(text, args.filter):
+
+            # handle the filter (if there is one)
+            if args.filter and not token_in_line(text, args.filter): # if there is a filter and the text does not pass it, skip it
                 continue
-            print(text)
+            if args.filter: # if there is a filter and the text passed it, count it
+                filteredCount += 1
+
+            # saving and printing the text
+            text_color = colorize(text)
+            print(text_color)
             if args.log:
                 with open(args.log, "a", encoding="utf-8") as logFile:
                     logFile.write(text + "\n")
+
+    # handle termination of the script
     except KeyboardInterrupt:
         ser.close()
         print(f"Serial connection closed. {messageCount} messages received, {errorCount} exceptions.")
+        if args.filter:
+            print(f"{filteredCount} messages passed the filter '{args.filter}'")
         return
     
 def main() -> None:
