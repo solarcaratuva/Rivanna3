915996151b2e0944eef796ca7ec86d2971e228bf
dt477 <domtr0510@gmail.com>
2025-08-31 16:37:11 -0400

    more formatting

--- upload.py
+++ upload.py
@@ -10,9 +10,9 @@ import os
 
 BOARD_MAP = {
     "power": "cmake_build/NUCLEO_F413ZH/develop/GCC_ARM/PowerBoard/PowerBoard.bin",
-    "telemetry": "cmake_build/POWER_BOARD/develop/GCC_ARM/TelemetryBoard/TelemetryBoard.bin", }
+    "telemetry": "cmake_build/POWER_BOARD/develop/GCC_ARM/TelemetryBoard/TelemetryBoard.bin",}
 
-HIL_BOARD_MAP = {}  # board path map specific to the HIL testing system server
+HIL_BOARD_MAP = {} # board path map specific to the HIL testing system server
 CMD_ARGS_ERASE = "-c port=SWD mode=UR -e all"
 CMD_ARGS_FLASH = "-c port=SWD mode=UR -rst -d"
 OS = platform.system()
@@ -22,19 +22,15 @@ EXE_PATH = r"C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\b
 def get_args():
     parser = argparse.ArgumentParser(
         description="Upload firmware to the board through a ST-Link")
-    parser.add_argument("board", type=str,
-                        help="Name of the board to upload firmware to")
-    parser.add_argument("-s", "--silent", action="store_true",
-                        help="Suppress upload output")
-    parser.add_argument("--hil", action="store_true",
-                        help="Upload firmware to the HIL testing system")
+    parser.add_argument("board", type=str, help="Name of the board to upload firmware to")
+    parser.add_argument("-s", "--silent", action="store_true", help="Suppress upload output")
+    parser.add_argument("--hil", action="store_true", help="Upload firmware to the HIL testing system")
     return parser.parse_args()
 
 
 def copy_file_to_windows(wsl_path: str) -> None:
     path = "/" + os.path.join("mnt", "c", "Windows", "Temp", "firmware.bin")
     shutil.copy(wsl_path, path)
-
 
 def is_wsl() -> bool:
     try:
@@ -50,13 +46,11 @@ def is_wsl() -> bool:
         pass
     return False
 
-
 def main() -> None:
     args = get_args()
     board = args.board.lower().replace("board", "")
     if board not in BOARD_MAP:
-        print(f"ERROR: '{args.board}' is not a valid board. Valid boards are [{
-              ', '.join(BOARD_MAP)}]")
+        print(f"ERROR: '{args.board}' is not a valid board. Valid boards are [{', '.join(BOARD_MAP)}]")
         sys.exit(1)
 
     match OS:
@@ -64,19 +58,15 @@ def main() -> None:
             if not is_wsl():  # non-WSL linux
                 path = BOARD_MAP[board]
                 cmd_erase = f"STM32_Programmer_CLI {CMD_ARGS_ERASE}"
-                cmd_flash = f"STM32_Programmer_CLI {
-                    CMD_ARGS_FLASH} {path} 0x08000000"
+                cmd_flash = f"STM32_Programmer_CLI {CMD_ARGS_FLASH} {path} 0x08000000"
             else:  # actually WSL in Windows
                 copy_file_to_windows(BOARD_MAP[board])
-                cmd_erase = f"powershell.exe \"& '{
-                    EXE_PATH}' {CMD_ARGS_ERASE}\""
-                cmd_flash = f"powershell.exe \"& '{EXE_PATH}' {
-                    CMD_ARGS_FLASH} C:\\Windows\\Temp\\firmware.bin 0x08000000\""
+                cmd_erase = f"powershell.exe \"& '{EXE_PATH}' {CMD_ARGS_ERASE}\""
+                cmd_flash = f"powershell.exe \"& '{EXE_PATH}' {CMD_ARGS_FLASH} C:\\Windows\\Temp\\firmware.bin 0x08000000\""
         case "Darwin":  # Mac:
             path = BOARD_MAP[board]
             cmd_erase = f"STM32_Programmer_CLI {CMD_ARGS_ERASE}"
-            cmd_flash = f"STM32_Programmer_CLI {
-                CMD_ARGS_FLASH} {path} 0x08000000"
+            cmd_flash = f"STM32_Programmer_CLI {CMD_ARGS_FLASH} {path} 0x08000000"
         case "Windows":
             print("ERROR: run this command in WSL")
             sys.exit(2)
@@ -84,12 +74,10 @@ def main() -> None:
             print("ERROR: Unknown OS")
             sys.exit(3)
 
-    process_erase = subprocess.run(
-        cmd_erase, capture_output=args.silent, check=False, shell=True)
+    process_erase = subprocess.run(cmd_erase, capture_output=args.silent, check=False, shell=True)
     if process_erase.returncode != 0:
         sys.exit(process_erase.returncode)
-    process_flash = subprocess.run(
-        cmd_flash, capture_output=args.silent, check=False, shell=True)
+    process_flash = subprocess.run(cmd_flash, capture_output=args.silent, check=False, shell=True)
     sys.exit(process_flash.returncode)
 
 
