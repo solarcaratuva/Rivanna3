7b33d722be264e4c9ee51ba2fb94575d2d779983
dt477 <domtr0510@gmail.com>
2025-08-31 16:28:54 -0400

    fixed formatting

--- upload.py
+++ upload.py
@@ -10,9 +10,9 @@ import os
 
 BOARD_MAP = {
     "power": "cmake_build/NUCLEO_F413ZH/develop/GCC_ARM/PowerBoard/PowerBoard.bin",
-    "telemetry": "cmake_build/POWER_BOARD/develop/GCC_ARM/TelemetryBoard/TelemetryBoard.bin",}
+    "telemetry": "cmake_build/POWER_BOARD/develop/GCC_ARM/TelemetryBoard/TelemetryBoard.bin", }
 
-HIL_BOARD_MAP = {} # board path map specific to the HIL testing system server
+HIL_BOARD_MAP = {}  # board path map specific to the HIL testing system server
 CMD_ARGS_ERASE = "-c port=SWD mode=UR -e all"
 CMD_ARGS_FLASH = "-c port=SWD mode=UR -rst -d"
 OS = platform.system()
@@ -20,36 +20,34 @@ EXE_PATH = r"C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\b
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Upload firmware to the board through a ST-Link")
-    parser.add_argument("board", type=str, help="Name of the board to upload firmware to")
-    parser.add_argument("-s", "--silent", action="store_true", help="Suppress upload output")
-    parser.add_argument("--hil", action="store_true", help="Upload firmware to the HIL testing system")
+    parser = argparse.ArgumentParser(
+        description="Upload firmware to the board through a ST-Link")
+    parser.add_argument("board", type=str,
+                        help="Name of the board to upload firmware to")
+    parser.add_argument("-s", "--silent", action="store_true",
+                        help="Suppress upload output")
+    parser.add_argument("--hil", action="store_true",
+                        help="Upload firmware to the HIL testing system")
     return parser.parse_args()
+
 
 def copy_file_to_windows(wsl_path: str) -> None:
     path = "/" + os.path.join("mnt", "c", "Windows", "Temp", "firmware.bin")
     shutil.copy(wsl_path, path)
 
+
 def is_wsl() -> bool:
-    """
-    Returns True if running inside Windows Subsystem for Linux (WSL), False otherwise.
-    """
     try:
         # Check uname release for "microsoft" (common in WSL1/WSL2)
         if "microsoft" in platform.uname().release.lower():
             return True
-        
-        # WSL2 often exposes this env var
         if "WSL_INTEROP" in os.environ:
             return True
-        
-        # Check /proc/version for "Microsoft"
-        with open("/proc/version", "r") as f:
+        with open("/proc/version", "r") as f:  # Check /proc/version for "Microsoft"
             if "microsoft" in f.read().lower():
                 return True
     except Exception:
         pass
-    
     return False
 
 
@@ -57,23 +55,28 @@ def main() -> None:
     args = get_args()
     board = args.board.lower().replace("board", "")
     if board not in BOARD_MAP:
-        print(f"ERROR: '{args.board}' is not a valid board. Valid boards are [{', '.join(BOARD_MAP)}]")
+        print(f"ERROR: '{args.board}' is not a valid board. Valid boards are [{
+              ', '.join(BOARD_MAP)}]")
         sys.exit(1)
 
     match OS:
-        case "Linux": # actually WSL in Windows 
-            if not is_wsl():
+        case "Linux":
+            if not is_wsl():  # non-WSL linux
                 path = BOARD_MAP[board]
                 cmd_erase = f"STM32_Programmer_CLI {CMD_ARGS_ERASE}"
-                cmd_flash = f"STM32_Programmer_CLI {CMD_ARGS_FLASH} {path} 0x08000000"
-            else: 
+                cmd_flash = f"STM32_Programmer_CLI {
+                    CMD_ARGS_FLASH} {path} 0x08000000"
+            else:  # actually WSL in Windows
                 copy_file_to_windows(BOARD_MAP[board])
-                cmd_erase = f"powershell.exe \"& '{EXE_PATH}' {CMD_ARGS_ERASE}\""
-                cmd_flash = f"powershell.exe \"& '{EXE_PATH}' {CMD_ARGS_FLASH} C:\\Windows\\Temp\\firmware.bin 0x08000000\""
-        case "Darwin": # Mac:
+                cmd_erase = f"powershell.exe \"& '{
+                    EXE_PATH}' {CMD_ARGS_ERASE}\""
+                cmd_flash = f"powershell.exe \"& '{EXE_PATH}' {
+                    CMD_ARGS_FLASH} C:\\Windows\\Temp\\firmware.bin 0x08000000\""
+        case "Darwin":  # Mac:
             path = BOARD_MAP[board]
             cmd_erase = f"STM32_Programmer_CLI {CMD_ARGS_ERASE}"
-            cmd_flash = f"STM32_Programmer_CLI {CMD_ARGS_FLASH} {path} 0x08000000"
+            cmd_flash = f"STM32_Programmer_CLI {
+                CMD_ARGS_FLASH} {path} 0x08000000"
         case "Windows":
             print("ERROR: run this command in WSL")
             sys.exit(2)
@@ -81,12 +84,13 @@ def main() -> None:
             print("ERROR: Unknown OS")
             sys.exit(3)
 
-    process_erase = subprocess.run(cmd_erase, capture_output=args.silent, check=False, shell=True)
+    process_erase = subprocess.run(
+        cmd_erase, capture_output=args.silent, check=False, shell=True)
     if process_erase.returncode != 0:
         sys.exit(process_erase.returncode)
-    process_flash = subprocess.run(cmd_flash, capture_output=args.silent, check=False, shell=True)
+    process_flash = subprocess.run(
+        cmd_flash, capture_output=args.silent, check=False, shell=True)
     sys.exit(process_flash.returncode)
-
 
 
 if __name__ == "__main__":
