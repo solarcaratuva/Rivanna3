818636b4c5c4f1106c4255115de96f33feb7d539
Colby Wise <cwise1080@gmail.com>
2025-12-04 19:39:26 -0500

    Added tests

--- PowerBoard/tests/test_reset_can.py
+++ PowerBoard/tests/test_reset_can.py
@@ -8,6 +8,7 @@ testing_lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..",
 sys.path.insert(0, testing_lib_path)
 
 from gpioPins import reset_nucleo
+from CANBus import CANBus
 
 
 class TestPowerBoardBasic(unittest.TestCase):
@@ -15,17 +16,17 @@ class TestPowerBoardBasic(unittest.TestCase):
     def test_setUp(self):
         # reset Nucleo before each test
         reset_nucleo()
-        printf("Reset nucleo")
+        print("Reset nucleo")
 
         # clear CAN messages
         self.bus = CANBus()
         for msg in self.bus.CANBusDict.keys():
             # reset can queue
             self.bus.clearReceivedMessages(msg)
-        #printf("Cleared CAN messages")
+        print("Cleared CAN messages")
 
         for msg in self.bus.CANBusDict:
-            self.assertEqual(len(bus.getReceivedMessages(msg)), 0, f"CAN queue '{msg}' not empty after clearing")
+            self.assertEqual(len(self.bus.getReceivedMessages(msg)), 0, f"CAN queue '{msg}' not empty after clearing")
 
 
         # Give firmware time to reboot
