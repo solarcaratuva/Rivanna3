ebd235bdc06fe15b2804db81ea6d4d1182faa872
abhinavp5 <abhinavpappu05@gmail.com>
2026-02-01 15:20:42 -0500

    uncommented loop in main

--- PowerBoard/src/main.cpp
+++ PowerBoard/src/main.cpp
@@ -224,10 +224,10 @@ int main() {
             ThisThread::sleep_for(100);
             motor_interface.sendThrottle(128);
             ThisThread::sleep_for(FLASH_PERIOD);
-            // for(int i = 0; i <= 256; i += 32) {
-            //     motor_interface.sendThrottle(i);
-            //     ThisThread::sleep_for(FLASH_PERIOD);
-            // } 
+            for(int i = 0; i <= 256; i += 32) {
+                motor_interface.sendThrottle(i);
+                ThisThread::sleep_for(FLASH_PERIOD);
+            } 
         } else if (I2C_TEST_MODE ==1){
             // Test regen values from 0 to 256
             // for(int i =0; i <= 256; i += 32) {
