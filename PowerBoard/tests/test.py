6b428699f0125199cfe561160565efcfbb804d23
abhinavp5 <abhinavpappu05@gmail.com>
2026-02-08 15:39:38 -0500

    turning pin on

--- PowerBoard/tests/test.py
+++ PowerBoard/tests/test.py
@@ -121,6 +121,7 @@ class DriverBoardTests(unittest.TestCase):
 
         for tv in testing_voltages:
             throttle_pin.write_voltage(tv)  
+            throttle_pin.on()
             time.sleep(0.5)  # Allow time for PowerBoard to read voltage, send I2C, Arduino to process and send Serial
             exp_norm, exp_raw = expected_throttle_value(tv)
             norm, raw = motor_interface.get_throttle(), motor_interface.get_throttle_raw()
@@ -172,6 +173,7 @@ class DriverBoardTests(unittest.TestCase):
         testing_voltages = [1.75] # 0.85V => ~3 raw => High Regen
         for tv in testing_voltages:
             throttle_pin.write_voltage(tv)
+            throttle_pin.on()
             time.sleep(0.5)
 
             exp_norm, exp_raw = expected_regen_from_throttle(tv)
